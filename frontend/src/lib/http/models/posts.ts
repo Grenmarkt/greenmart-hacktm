eb4c2bf93cb0b8f4d6e581fd6123d2f1e7d016c9
biapopica <popicabeatrica@outlook.com>
2025-05-31 14:20:47 +0300

    add create product

--- frontend/src/lib/http/models/posts.ts
+++ frontend/src/lib/http/models/posts.ts
@@ -11,4 +11,15 @@ export const postOutputSchema = z.object({
   description: z.string().nullable(),
 });
 
+export const createProductInputSchema = z.object({
+  productType: z.string(),
+  title: z.string(),
+  description: z.string().optional(),
+  price: z.number(),
+  unitType: z.string(),
+  stock: z.number(),
+  imageUrl: z.string().optional(),
+  imageId: z.string().optional(),
+});
+
 export type PostInput = z.infer<typeof postInputSchema>;
