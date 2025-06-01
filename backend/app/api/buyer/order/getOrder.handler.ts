89cb29f03593be48d222bba2d86a9c4d76f89312
george1586 <george_ichim101@yahoo.com>
2025-05-31 20:00:17 -0400

    getAllOrders

---
+++ backend/app/api/buyer/order/getOrder.handler.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from 'express';
+import { prismaClient } from '../../../db/prismaClient.ts';
+import type { AuthLocals } from '../../../middleware/authHandlers.ts';
+export const getOrderProducts= async(
+    req: Request,
+    res: Response<unknown, AuthLocals>) =>
+{
+    const {user} = res.locals;
+
+    const order = await prismaClient.order.findMany({
+        where: {
+            userId: user.id,
+            status: 'PENDING'
+        }
+    });
+
+    const orderProducts = await Promise.all(order.map(async (order) => {
+            return await prismaClient.orderProduct.findMany({
+                where: {
+                    orderId: order.id
+                },
+                include:
+                {
+                    product: true
+                }
+            });
+        }));
+
+    
+    res.status(202).json(orderProducts);
+}
--- backend/app/api/buyer/order/order.routes.ts
+++ backend/app/api/buyer/order/order.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { authHandler } from '../../../middleware/authHandlers.ts';
 import { createOrder } from './createOrder.handlers.ts';
 import { deleteOrder } from './deleteOrder.handler.ts';
+import { getOrderProducts } from './getOrder.handler.ts';
 
 const router = Router();
 
 router.post('/',authHandler,createOrder);
 router.delete('/:orderId',authHandler,deleteOrder);
+router.get('/',authHandler,getOrderProducts);
 
 export { router as orderRouter };
